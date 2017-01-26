30ce1626ab6ca454cc0e293f736ff26d9bebdc21
patrick <pgourdet@ufl.edu>
2017-01-25 21:53:09 -0500

    First Commit Finished Assignment4

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+//This file holds any configuration variables we may need 
+module.exports = {
+  db: {
+      uri: 'mongodb://pat:Infinity9@ds131119.mlab.com:31119/ufproject1', //place the URI of your mongo database here.
+  },
+  googleMaps: {
+    key: ''
+  },
+  port: 8080
+};
+
+
